0cab7e1af962c4e5cbc24d0d1d756a7d1fca4da4
xqzh <xqzh@ideabinder.com>
2016-12-05 14:09:08 +0800

    占比图增加方向控制

--- DRHistogramViewExample/DRHistogramView.swift
+++ DRHistogramViewExample/DRHistogramView.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum Direction {
+  case left
+  case right
+}
+
 class DRHistogramView: UIView {
   
   var histogram:CAShapeLayer!   = CAShapeLayer()  // 占用条
@@ -15,6 +20,7 @@ class DRHistogramView: UIView {
   var textLayer:HistogramLayer! = HistogramLayer()// 文字
   var path:CGPath!                                // 路径
   var text:NSString!                              // 显示
+  var direction:Direction!      = .right           // 控件动画方向,默认往右
   var proportion:CGFloat! {                       // 占比
     didSet {
       self.setNeedsDisplay()
@@ -23,6 +29,7 @@ class DRHistogramView: UIView {
   
   override init(frame: CGRect) {
     super.init(frame: frame)
+    
     self.layer.addSublayer(backHisto)
     self.layer.addSublayer(histogram)
     self.layer.addSublayer(textLayer)
@@ -46,9 +53,9 @@ class DRHistogramView: UIView {
     
     let x                     = (self.bounds.size.width - 70) / 2.0
     textLayer.frame           = CGRect(x: x, y: 12, width: 70, height: 26)
-    let red:CGFloat           = 135/255.0
-    let blue:CGFloat          = 135/255.0
-    let green:CGFloat         = 135/255.0
+    let red:CGFloat           = 255/255.0
+    let blue:CGFloat          = 255/255.0
+    let green:CGFloat         = 255/255.0
     textLayer.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 0.5).cgColor
     textLayer.borderColor     = UIColor.lightGray.cgColor
     textLayer.bounds          = CGRect(x:0, y:12,width: 70, height: 26);
@@ -61,10 +68,18 @@ class DRHistogramView: UIView {
       // 记录原先的path
       let oldPath = path
       // 生成新的path
-      path = CGPath(roundedRect: CGRect(x: 0, y: 0, width: proportion, height: 50),
-                    cornerWidth: 5,
-                    cornerHeight: 5,
-                    transform: nil)
+      if direction == .right {
+        path = CGPath(roundedRect: CGRect(x: 0, y: 0, width: proportion, height: 50),
+                      cornerWidth: 5,
+                      cornerHeight: 5,
+                      transform: nil)
+      }
+      else {
+        path = CGPath(roundedRect: CGRect(x: 200 - proportion, y: 0, width: proportion, height: 50),
+                      cornerWidth: 5,
+                      cornerHeight: 5,
+                      transform: nil)
+      }
       histogram.path = path
       
       // 加CABasicAnimation动画
