ef041e6d136a5dad463301465bd43f20b1c27e78
xqzh <xqzh@ideabinder.com>
2016-12-07 11:57:34 +0800

    增加内存占用计算方法

---
+++ DRHistogramViewExample/MemoryCaculate.h
@@ -0,0 +1,15 @@
+//
+//  MemoryCaculate.h
+//  DRHistogramViewExample
+//
+//  Created by xqzh on 16/12/7.
+//  Copyright © 2016年 xqzh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MemoryCaculate : NSObject
+
+- (double)availableMemory;
+- (double)usedMemory;
+@end
